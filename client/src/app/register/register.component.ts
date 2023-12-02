c4412514e40110a3ddb1e2c953eda61bf40a0cc7
Rakesh <chipparakesh01@gmail.com>
2023-12-02 19:42:21 +0530

    add Accordion

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,16 +9,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
+  ngOnInit(): void {
 
+    this.registerForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required),
+      designation: new FormControl(null, Validators.required),
+      department: new FormControl(null, Validators.required),
+      mobile: new FormControl(null, Validators.required)
+    });
 
-  // this.registerForm = new FormGroup({
-  //   username: new FormControl(null, Validators.required),
-  //   password: new FormControl(null, Validators.required),
-  //   email: new FormControl(null, Validators.required),
-  //   designation: new FormControl(null, Validators.required),
-  //   department: new FormControl(null, Validators.required),
-  //   mobile: new FormControl(null, Validators.required)
-  // });
+  }
+
 }
