f9235b6481a1844787c13203b5c371286b41649f
Rakesh <chipparakesh01@gmail.com>
2023-12-02 18:29:55 +0530

    Added navigation to login and register components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UserComponent } from "./user/user.component";
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';;
 import { AppService } from './app.service';
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, RouterOutlet, NavbarComponent, UserComponent]
+  imports: [CommonModule, RouterOutlet, NavbarComponent, LoginComponent, RegisterComponent]
 })
 export class AppComponent implements OnInit {
   title = 'client';
   isUserLoggedIn = false
 
 
-  constructor(private service: AppService) { }
+  constructor(private service: AppService, private router: Router) { }
+
   ngOnInit() {
+    this.navigateUser()
     this.service.isUserLoggedIn$.subscribe(res => {
       this.isUserLoggedIn = res
+      this.navigateUser();
     })
   }
 
-
+  private navigateUser() {
+    if (this.isUserLoggedIn) {
+      this.router.navigate(['/home']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MachineRollComponent } from './machine-roll/machine-roll.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: HomePageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: "login", pathMatch: "full" },
+
     { path: 'machineRoll', component: MachineRollComponent },
 ];
