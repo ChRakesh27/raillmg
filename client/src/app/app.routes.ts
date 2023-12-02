bd71388fd50d5438f557ae961ebe03bf2628c607
Rakesh <chipparakesh01@gmail.com>
2023-12-02 13:56:05 +0530

    add navbar component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { MachineRollComponent } from './machine-roll/machine-roll.component';
 
 export const routes: Routes = [
     { path: 'home', component: UserComponent },
-    { path: '', component: HomePageComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'machineRoll', component: MachineRollComponent },
 ];
