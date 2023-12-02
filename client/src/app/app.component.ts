36d476f3bf3cf6e1630e5c5c690ac35baa4d766f
Rakesh <chipparakesh01@gmail.com>
2023-12-02 15:02:42 +0530

    add navbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, UserComponent]
 })
 export class AppComponent {
   title = 'client';
+  isUserLoggedIn = true
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MachineRollComponent } from './machine-roll/machine-roll.component';
 
 export const routes: Routes = [
-    { path: 'home', component: UserComponent },
     { path: '', component: HomePageComponent },
     { path: 'machineRoll', component: MachineRollComponent },
 ];
