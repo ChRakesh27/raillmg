911d66606f9cd5f842ef27a77994800ff6cad51e
Rakesh <chipparakesh01@gmail.com>
2023-12-08 18:37:26 +0530

    add maintenance  roll component

---
+++ client/src/app/lmg/demand/maintenance-roll/maintenance-roll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-roll',
+  standalone: true,
+  imports: [],
+  templateUrl: './maintenance-roll.component.html',
+  styleUrl: './maintenance-roll.component.css'
+})
+export class MaintenanceRollComponent {
+
+}
