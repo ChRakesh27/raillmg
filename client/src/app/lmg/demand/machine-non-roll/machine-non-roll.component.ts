d6813a0bb699db8b39b9d89d291c9032ee78b142
Rakesh <chipparakesh01@gmail.com>
2023-12-08 18:36:31 +0530

    add machine non roll component

---
+++ client/src/app/lmg/demand/machine-non-roll/machine-non-roll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-non-roll',
+  standalone: true,
+  imports: [],
+  templateUrl: './machine-non-roll.component.html',
+  styleUrl: './machine-non-roll.component.css'
+})
+export class MachineNonRollComponent {
+
+}
