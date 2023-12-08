beb582342f629d9a5ea0ca3b818bf5194a858282
Rakesh <chipparakesh01@gmail.com>
2023-12-08 18:37:04 +0530

    add maintenance non roll component

---
+++ client/src/app/lmg/demand/maintenance-non-roll/maintenance-non-roll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-non-roll',
+  standalone: true,
+  imports: [],
+  templateUrl: './maintenance-non-roll.component.html',
+  styleUrl: './maintenance-non-roll.component.css'
+})
+export class MaintenanceNonRollComponent {
+
+}
