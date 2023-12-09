466c633428b1941b027046725ff34910199eb2ae
Rakesh <chipparakesh01@gmail.com>
2023-12-09 15:09:37 +0530

    removed ssr

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
@@ -11,12 +10,10 @@ import {
 } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpInterceptorService } from './http-interceptor.service';
-// import { intercept } from './http-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(
       withFetch(),
       withInterceptorsFromDi()
