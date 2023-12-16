60c5323b1901b4ba7bdaaebb6394548b73b639fe
Rakesh <chipparakesh01@gmail.com>
2023-12-16 18:54:57 +0530

    Added express sever in angular

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 const express = require("express")
 const cors = require("cors");
-const apiRoutes = require("./routes/api.routes");
+var morgan = require('morgan')
 
-require("dotenv").config();
+const apiRoutes = require("./routes/api.routes");
 
 require("./config/db")
 
 const app = express()
+app.use(morgan('tiny'))
 app.use(express.json({ limit: "1mb" }));
 app.use(express.json())
 app.use(cors())
 
 app.use('/api', apiRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 
