50313807173c71b383b72ac2b9d0a1ec0bd3fac4
Rakesh <chipparakesh01@gmail.com>
2024-02-23 18:30:41 +0530

    new build

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.json())
 app.use(cors())
 
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 // app.get('*.*', express.static(path.join(__dirname, APP_DIR)));
 // app.all('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, APP_DIR, 'index.html'))
